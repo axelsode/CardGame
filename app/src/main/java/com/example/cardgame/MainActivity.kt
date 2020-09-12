5a08f4560a543cc5715aa3739955c9fc14b59ba4
daahman <58550693+daahman@users.noreply.github.com>
2020-09-12 09:22:24 +0200

    moved Card
    missade att kola att Card hamnade rätt och flyttade den. lagt till 4 kort i
    huvud viewn var av ett synligt.

--- app/src/main/java/com/example/cardgame/MainActivity.kt
+++ app/src/main/java/com/example/cardgame/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.cardgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dealerCar1 = findViewById<ImageView>(R.id.dealer1)
+        val dealerCar2 = findViewById<ImageView>(R.id.dealer2)
+        val dealerCar3 = findViewById<ImageView>(R.id.dealer3)
+        val dealerCar4 = findViewById<ImageView>(R.id.dealer4)
+
+        dealerCar1.visibility = View.VISIBLE
+        dealerCar2.visibility = View.INVISIBLE
+        dealerCar3.visibility = View.INVISIBLE
+        dealerCar4.visibility = View.INVISIBLE
+
+
     }
 }
