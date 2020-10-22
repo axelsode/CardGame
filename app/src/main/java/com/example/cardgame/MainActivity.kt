41a2c25f56f6e70e6ba3f7edba381d6dfd114a5b
axelsode <axel.soderber1996@gmai.com>
2020-10-22 14:31:17 +0200

    change end text to black

--- app/src/main/java/com/example/cardgame/MainActivity.kt
+++ app/src/main/java/com/example/cardgame/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 
 class MainActivity : AppCompatActivity() {
